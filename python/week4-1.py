4ae67ba50e3423a8da66dd2c76873da4f225e57f
wijdan1969 <wueen.fatima@yahoo.com>
2018-11-02 21:35:20 +0400

    Add week4

---
+++ python/week4-1.py
@@ -0,0 +1,7 @@
+print(input('Enter a number:'))
+num =7
+if num % 2 == 0:
+
+    print('even')
+else:
+    print('odd')
---
+++ python/week4-2.py
@@ -0,0 +1,8 @@
+print("Numbers from 1 to 10")
+print(input("Guess the number: "))
+num =15
+if num >= 1:
+    if num <= 10:
+        print('Great you did it!')
+    if num > 10:
+        print('Try again')
